bc29d45a328152c9c5e95cb0397f9fab873a352e
Drew Wiskus <andrewwiskus@gmail.com>
2017-01-05 11:05:51 -0600

    filters are great

--- Map_Reduce_Filter.playground/Contents.swift
+++ Map_Reduce_Filter.playground/Contents.swift
@@ -8,7 +8,7 @@ var strArray = ["a","b","c","d","e"]
 let squaresPlusNum = intArray.map {value -> Int in
     var number = 123
     return value * value + number
- }
+}
 
 let onlyEvens = intArray.filter {value in
     var number = 123
@@ -17,3 +17,14 @@ let onlyEvens = intArray.filter {value in
 print("Squared + Num: \(squaresPlusNum)")
 print("Filtered for Evens: \(onlyEvens)")
 
+
+let taskNames = ["first", "second", "all", "unique", "taskStrings"]
+
+func removeTask(item: String, from: [String]) -> [String] {
+    let filteredArray = from.filter({value in
+    return value != item
+    })
+    return filteredArray
+}
+
+let newArray = removeTask(item: "second", from: taskNames)
